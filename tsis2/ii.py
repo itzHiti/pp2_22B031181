afaf4e5ca1029f3054c91ff1ba9f9506c53a540b
itzHiti <karim_madi@mail.ru>
2023-02-04 23:37:17 +0600

    Created tsis2 directory
    Added all tasks from TSIS_2.pdf

---
+++ tsis2/ii.py
@@ -0,0 +1,19 @@
+thislist = ["apple", "banana", "cherry"]
+print(thislist)
+
+thislist = ["apple", "banana", "cherry", "apple", "cherry"]
+print(thislist)
+
+thislist = ["apple", "banana", "cherry"]
+print(len(thislist))
+
+list1 = ["apple", "banana", "cherry"]
+list2 = [1, 5, 7, 9, 3]
+list3 = [True, False, False]
+
+list1 = ["abc", 34, True, 40, "male"]
+mylist = ["apple", "banana", "cherry"]
+print(type(mylist))
+
+thislist = list(("apple", "banana", "cherry")) # note the double round-brackets
+print(thislist)
