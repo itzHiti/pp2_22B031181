5a11a5c629f67621b8b6eb9ee05f2b5fc31a7011
itzHiti <karim_madi@mail.ru>
2023-03-07 15:47:04 +0600

    Updated tsis6 directory
    Added some useful code in vi.py and changed a little bit viii.py

--- tsis6/dir-and-files/vi.py
+++ tsis6/dir-and-files/vi.py
@@ -10,3 +10,11 @@ for i in range(65,91):
     f.write("this is just regular file")
     f.close()
 termcolor.cprint("Files from A to Z created succesfully.", "green", attrs=["bold"])
+"""
+n = int(input('To delete all files type "1": '))
+if n == 1:
+    for i in range(65, 91):
+        if os.path.exists(os.path.join(p, "{0}.txt".format(chr(i))) ):
+            os.remove(os.path.join(p, "{0}.txt".format(chr(i))))
+termcolor.cprint("Files from A to Z terminated.", "red")
+"""
--- tsis6/dir-and-files/viii.py
+++ tsis6/dir-and-files/viii.py
@@ -4,8 +4,10 @@ d_p = os.getcwd()
 s = input("Path: ")
 f = input("File (w/ extension): ")
 e = os.path.exists(os.path.join(os.path.join(d_p, s), f))
-if(e == True):
+if (os.access(os.path.join(os.path.join(d_p, s), f), os.R_OK), e == True):
     os.remove(os.path.join(os.path.join(d_p, s), f))
     termcolor.cprint("File deleted.", "yellow")
 else:
     termcolor.cprint("Oops... There is no such file (maybe you already deleted it).", "red")
+# example: tsis6/dir-and-files/ddd
+# "LETTER".txt
