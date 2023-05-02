972e6bef06952a5e70c39eca02a2a7cdbc5cccc8
itzHiti <karim_madi@mail.ru>
2023-05-02 22:38:47 +0600

    Added tsis11
    Updated task from tsis10

--- tsis10/phonebook.py
+++ tsis10/phonebook.py
@@ -1,9 +1,11 @@
+# I HATE THIS DB, WHY NOT MYSQL OR MARIADB?
+
+
 # Import the necessary libraries
-import psycopg2
-import csv
+import psycopg2, csv, os, time, sys
 
 # Establish connection to the PostgreSQL database
-conn = psycopg2.connect(database="PhoneBook", user="username", password="password", host="localhost", port="5432")
+conn = psycopg2.connect(database="PhoneBook", user="postgres", password="qwe", host="localhost", port="5432")
 cur = conn.cursor()
 
 # Create tables for PhoneBook
@@ -15,7 +17,7 @@ cur.execute('''CREATE TABLE IF NOT EXISTS users (
 
 # Insert data into the PhoneBook table from a csv file
 def insert_from_csv(filename):
-    with open(filename, 'r') as file:
+    with open("tsis10/"+filename, 'r') as file:
         reader = csv.reader(file)
         next(reader) # Skip header row
         for row in reader:
@@ -47,6 +49,159 @@ def delete_data(filter, value):
     cur.execute("DELETE FROM users WHERE {} = %s".format(filter), (value,))
     conn.commit()
 
-# Close the database connection
-cur.close()
-conn.close()
+# Create a function that returns all records based on a pattern
+def get_users_by_pattern(pattern):
+    cur.execute("SELECT * FROM users WHERE firstname LIKE %s OR lastname LIKE %s OR phone LIKE %s", (f"%{pattern}%", f"%{pattern}%", f"%{pattern}%"))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+# Create a procedure to insert new user by name and phone, update phone if user already exists
+def insert_or_update_user(firstname, lastname, phone):
+    cur.execute("SELECT COUNT(*) FROM users WHERE firstname = %s AND lastname = %s", (firstname, lastname))
+    count = cur.fetchone()[0]
+    if count == 0:
+        cur.execute("INSERT INTO users (firstname, lastname, phone) VALUES (%s, %s, %s)", (firstname, lastname, phone))
+    else:
+        cur.execute("UPDATE users SET phone = %s WHERE firstname = %s AND lastname = %s", (phone, firstname, lastname))
+    conn.commit()
+
+# Create a procedure to insert many new users by list of name and phone
+def insert_many_users(users):
+    incorrect_data = []
+    for user in users:
+        name, lastname, phone = user.split(',')
+        lastname = lastname.strip()
+        phone = phone.strip()
+        if len(phone) != 10 or not phone.isdigit():
+            incorrect_data.append(user)
+            continue
+        cur.execute("INSERT INTO users (firstname, lastname, phone) VALUES (%s, %s)", (name, lastname, phone))
+    conn.commit()
+    return incorrect_data
+
+# Create a function to querying data from the tables with pagination (by limit and offset)
+def query_data_with_pagination(filter, value, limit, offset):
+    cur.execute("SELECT * FROM users WHERE {} = %s LIMIT %s OFFSET %s".format(filter), (value, limit, offset))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+# Create a procedure to deleting data from tables by username or phone
+def delete_user_by_filter(filter, value):
+    cur.execute("DELETE FROM users WHERE {} = %s".format(filter), (value,))
+    conn.commit()
+
+def get_user_list():
+    user_list = []
+    while True:
+        user_data = input("Enter user data in the format 'firstname, lastname, phone' (or enter 'done' when finished): ")
+        if user_data.lower() == "done":
+            break
+        else:
+            firstname, lastname, phone = user_data.split(",")
+            user_list.append({"firstname": firstname.strip(), "lastname": lastname.strip(), "phone": phone.strip()})
+    return user_list
+
+# For console cleaning
+clear = lambda: os.system("cls")
+
+# Empty string
+val = ""
+
+while True:
+    clear()
+    print(val)
+    option = input(
+    """
+    Choose what option you want to use:
+
+    [1] - Insert from CSV
+    [2] - Insert from Console
+    [3] - Update data
+    [4] - Query data
+    [5] - Delete data
+    [6] - Search by pattern
+    [7] - Insert or update user
+    [8] - Insert many new users
+    [9] - Query data with pagination (by limit and offset)
+    [10] - Delete data, but as number 10
+
+    OR
+
+    [0] - To leave
+
+    """)
+
+    match (int(option)):
+        case 1:
+            clear()
+            fn = input("Input file name: ")
+            insert_from_csv(fn)
+            val = "Your .csv file's values were imported to database."
+        case 2:
+            clear()
+            insert_from_console()
+            val = "Your values were imported to database."
+        case 3:
+            clear()
+            fn = input("Enter firstname: ")
+            ln = input("Enter lastname: ")
+            npn = input("Enter new phone number: ")
+            update_data(fn, ln, npn)
+            val = "Old data from database were updated with new one."
+        case 4:
+            clear()
+            time_to_wait = input("Type time (in seconds) to wait for your data to load: ")
+            filter = input("Enter by what filter data should be searched (firstname, lastname or phone): ")
+            value = input("Enter filter's value: ")
+            query_data(filter=filter, value=value)
+            time.sleep(int(time_to_wait))
+        case 5:
+            clear()
+            filter = input("Enter by what filter data should be searched to be deleted (firstname, lastname or phone): ")
+            value = input("Enter filter's value: ")
+            delete_data(filter=filter, value=value)
+            val = "Deleted data from database."
+        case 6:
+            clear()
+            value = input("Enter pattern's value: ")
+            get_users_by_pattern(value)
+            val = "Showing all data found by pattern " + value
+        case 7:
+            clear()
+            value = input("Enter pattern's value: ")
+            get_users_by_pattern(value)
+            val = "Old phone number from database was updated with new one."
+        case 8:
+            clear()
+            users = get_user_list()
+            insert_many_users(users)
+            val = "Added many users to database"
+        case 9:
+            clear()
+            filter = input("Enter by what filter data should be searched (firstname, lastname or phone): ")
+            value = input("Enter filter's value: ")
+            l = input("Enter limit: ")
+            o = input("Enter offset: ")
+            query_data_with_pagination(filter=filter, value=value, limit=l, offset=o)
+            val = ""
+        case 10:
+            clear()
+            filter = input("Enter by what filter data should be searched (firstname, lastname or phone): ")
+            value = input("Enter filter's value: ")
+            delete_user_by_filter(filter=filter, value=value)
+            val = ""
+        case 0:
+            clear()
+            clear()
+            print("Thank you and bye :)")
+            time.sleep(3)
+            # Close the database connection
+            cur.close()
+            conn.close()
+            sys.exit()
+        case _:
+            val = ("""
+            Choose number from 1 to 5
+            """)
---
+++ tsis10/sanek.py
@@ -0,0 +1,230 @@
+# Media files taken from "Snake III" game 
+
+import pygame, random, time, sys, psycopg2
+from threading import Timer
+
+pygame.init()
+    
+# Window resolution (HD)
+res = w, h = 700, 700
+
+# FPS
+FPS = 60
+FramePerSec = pygame.time.Clock()
+
+# Predefined some colors
+BLUE  = (0, 0, 255)
+RED   = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+
+# Some variables
+SPEED = 10
+SCORE = 0
+LEVEL = 0
+x = 0
+
+# Establish connection to the PostgreSQL database
+conn = psycopg2.connect(database="Snake", user="postgres", password="qwe", host="localhost", port="5432")
+cur = conn.cursor()
+
+# Create tables for PhoneBook
+cur.execute('''CREATE TABLE IF NOT EXISTS users (
+                id SERIAL PRIMARY KEY,
+                nickname TEXT NOT NULL,
+                score TEXT NOT NULL)''')
+
+# Creating a green screen and caption of game
+pygame.display.set_caption("Snakes III?")
+pygame.display.set_icon(pygame.image.load("tsis8/snake/icon.png"))
+DISPLAYSURF = pygame.display.set_mode((res))
+
+startdirection = 'RIGHT'
+direction = startdirection
+
+#sanek
+snake_pos = [50, 50]
+snake_body = [[50, 50],[40, 50]]
+
+# fruit
+fruit = pygame.transform.scale(pygame.image.load("tsis8/snake/food.png"), (20, 10))
+fruit_pos = [random.randrange(1, (w//10)) * 10, random.randrange(1, (h//10)) * 10]
+fruit_spawn = True
+# fruit2
+fruit2 = pygame.transform.scale(pygame.image.load("tsis8/snake/food2.png"), (20, 10))
+fruit2_pos = [random.randrange(1, (w//10)) * 10, random.randrange(1, (h//10)) * 10]
+fruit2_spawn = True
+# fruit3
+fruit3 = pygame.transform.scale(pygame.image.load("tsis8/snake/food3.png"), (20, 10))
+fruit3_pos = [random.randrange(1, (w//10)) * 10, random.randrange(1, (h//10)) * 10]
+fruit3_spawn = True
+# fruit4
+fruit4 = pygame.transform.scale(pygame.image.load("tsis8/snake/food4.png"), (20, 10))
+fruit4_pos = [random.randrange(1, (w//10)) * 10, random.randrange(1, (h//10)) * 10]
+fruit4_spawn = True
+
+def score(SCORE, LEVEL):
+   
+    font = pygame.font.SysFont("Verdana", 15)
+     
+    score_surface = font.render("Score : " + str(SCORE), True, BLACK)
+    level_surface = font.render("Level : " + str(LEVEL), True, BLACK)
+    
+    score_rect = score_surface.get_rect()
+
+    DISPLAYSURF.blit(score_surface, score_rect)
+    DISPLAYSURF.blit(level_surface, (200, 0))
+
+def win():
+    DISPLAYSURF.fill(BLUE)
+    font = pygame.font.SysFont("Verdana", 15)
+    win_surface = font.render("You won! You are going to the level " + str(LEVEL), True, BLACK)
+    win_rect = win_surface.get_rect()
+    pygame.mixer.Sound("tsis8/snake/win.mp3").play()
+    DISPLAYSURF.blit(win_surface, win_rect)
+    pygame.display.flip()
+
+    time.sleep(6)
+    DISPLAYSURF.fill(GREEN)
+
+def game_over():
+    DISPLAYSURF.fill(RED)
+    font = pygame.font.SysFont("Verdana", 15)
+    game_over_surface = font.render("You lost! Your Score is: " + str(SCORE), True, BLACK)
+    game_over_rect = game_over_surface.get_rect()
+    game_over_rect.midtop = (w/2, h/3)
+    
+    pygame.mixer.Sound("tsis8/snake/gameover.mp3").play()
+    DISPLAYSURF.blit(game_over_surface, game_over_rect)
+    pygame.display.flip()
+
+    time.sleep(6)
+    pygame.quit()
+    sys.exit()
+
+def killfood():
+    fruit4_spawn = False
+
+LIFETIME = Timer(10, killfood)
+LIFETIME.start()
+
+username = input("Enter your name: ")
+cur.execute("SELECT * FROM players WHERE {} = %s", (username))
+players = cur.fetchall()
+print(players)
+if len(players) == 0:
+  	print("Sign up in the system")
+else:
+	SCORE = players[0][1]
+    LEVEL = players[0][2]
+
+# Game loop
+while True:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                direction = 'UP'
+            if event.key == pygame.K_DOWN:
+                direction = 'DOWN'
+            if event.key == pygame.K_LEFT:
+                direction = 'LEFT'
+            if event.key == pygame.K_RIGHT:
+                direction = 'RIGHT'
+    
+
+    
+    if direction == 'UP' and startdirection != 'DOWN':
+        startdirection = 'UP'
+    if direction == 'DOWN' and startdirection != 'UP':
+        startdirection = 'DOWN'
+    if direction == 'LEFT' and startdirection != 'RIGHT':
+        startdirection = 'LEFT'
+    if direction == 'RIGHT' and startdirection != 'LEFT':
+        startdirection = 'RIGHT'
+
+    
+    if startdirection == 'UP':
+        snake_pos[1] -= 10
+    if startdirection == 'DOWN':
+        snake_pos[1] += 10
+    if startdirection == 'LEFT':
+        snake_pos[0] -= 10
+    if startdirection == 'RIGHT':
+        snake_pos[0] += 10
+
+    
+    snake_body.insert(0, list(snake_pos))
+    
+    if snake_pos[0] == fruit_pos[0] and snake_pos[1] == fruit_pos[1]:
+        SCORE += 1
+        x+=1
+        fruit_spawn = False
+        pygame.mixer.Sound("tsis8/snake/newfood.mp3").play()
+    elif snake_pos[0] == fruit2_pos[0] and snake_pos[1] == fruit2_pos[1]:
+        SCORE += 2
+        x+=2
+        fruit2_spawn = False
+        pygame.mixer.Sound("tsis8/snake/newfood.mp3").play()
+    elif snake_pos[0] == fruit3_pos[0] and snake_pos[1] == fruit3_pos[1]:
+        SCORE += 1
+        x+=1
+        fruit3_spawn = False
+        pygame.mixer.Sound("tsis8/snake/newfood.mp3").play()
+    elif snake_pos[0] == fruit4_pos[0] and snake_pos[1] == fruit4_pos[1]:
+        SCORE += 3
+        x+=3
+        fruit4_spawn = False
+        LIFETIME = Timer(10, killfood)
+        LIFETIME.start()
+        pygame.mixer.Sound("tsis8/snake/newfood.mp3").play()
+    else:
+        snake_body.pop()
+
+    DISPLAYSURF.fill(GREEN)
+
+    if not fruit_spawn:
+        fruit_pos = [random.randrange(1, (w//10)) * 10, random.randrange(1, (h//10)) * 10]
+    fruit_spawn = True	
+    DISPLAYSURF.blit(fruit, (fruit_pos[0]-7.5, fruit_pos[1]-7.5))
+    if not fruit2_spawn:
+        fruit2_pos = [random.randrange(1, (w//10)) * 10, random.randrange(1, (h//10)) * 10]
+    fruit2_spawn = True	
+    DISPLAYSURF.blit(fruit2, (fruit2_pos[0]-7.5, fruit2_pos[1]-7.5))
+    if not fruit3_spawn:
+        fruit3_pos = [random.randrange(1, (w//10)) * 10, random.randrange(1, (h//10)) * 10]
+    fruit3_spawn = True	
+    DISPLAYSURF.blit(fruit3, (fruit3_pos[0]-7.5, fruit3_pos[1]-7.5))
+    if not fruit4_spawn:
+        fruit4_pos = [random.randrange(1, (w//10)) * 10, random.randrange(1, (h//10)) * 10]
+    fruit4_spawn = True	
+    DISPLAYSURF.blit(fruit4, (fruit4_pos[0]-7.5, fruit4_pos[1]-7.5))
+    
+    for pos in range(len(snake_body)):
+        if pos == 0:
+            pygame.draw.circle(DISPLAYSURF, RED, (snake_body[pos][0], snake_body[pos][1]), 7.5)
+        else:
+            pygame.draw.circle(DISPLAYSURF, BLUE, (snake_body[pos][0], snake_body[pos][1]), 7.5)
+
+    # border
+    if snake_pos[0] < 0 or snake_pos[0] > w-10:
+        game_over()
+    if snake_pos[1] < 0 or snake_pos[1] > h-10:
+        game_over()
+
+    # 5 fruits => next level + increase speed
+    if x>=5:
+        SPEED=SPEED+(LEVEL*3)
+        LEVEL+=1
+        x=0
+        win()
+    score(SCORE, LEVEL)
+
+    conn.commit()
+	cur.close()
+
+    pygame.display.update()
+    FramePerSec.tick(FPS)
